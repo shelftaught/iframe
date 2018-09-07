c15f5ff09e6c9ab60e3add1cb356f460499842d0
duncangarde <duncangarde@hotmail.com>
2018-09-07 15:06:12 +0100

    updating link to file to be at 1.1

--- iframe-setup.js
+++ iframe-setup.js
@@ -1,7 +1,7 @@
 function setupDynamicIframe() {
 	[
 	  'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js',
-	  'https://cdn.jsdelivr.net/gh/duncangarde/shelftaught-iframe-js/iframe-load-actions.js.min'
+	  'https://cdn.jsdelivr.net/gh/duncangarde/shelftaught-iframe-js@1.1/iframe-load-actions.js'
 	].forEach(function(src) {
 	  var script = document.createElement('script');
 	  script.src = src;
