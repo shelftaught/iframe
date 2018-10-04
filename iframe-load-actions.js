f29829503c9a0af9168316569aafa7e9b9e32938
duncangarde <duncangarde@hotmail.com>
2018-10-04 17:28:16 +0100

    making modal pop up outside of iframe in site

--- iframe-load-actions.js
+++ iframe-load-actions.js
@@ -13,4 +13,45 @@ var iframes = document.querySelectorAll("iframe");
 var i;
 for (i = 0; i < iframes.length; i++) {
     iframes[i].contentWindow.postMessage({type: 'event_trigger', event: 'LazyLoadCovers'},'*')
+};
+// functions for modal in site browser not in iframe
+// fade in element (modal)
+function appearElement (element) {
+	element.classList.add('open');
+	setTimeout(function() {
+    	element.classList.add('in');
+  	});
+};
+// close and delete the modal and parent
+function closeDeleteModal () {
+	var modal = document.getElementById('st-modal')
+	modal.classList.remove('in');
+	setTimeout(function() {
+    	modal.classList.remove('open');
+  	});	
+  	modal.parentElement.remove();
+  	document.body.classList.remove('st-modal-open')
 }
+// receive html from shelftaught through iframe postmessage
+function receiveMessage(event) {
+	console.log(event.origin);
+	if (event.origin === "https://shelftaught.com") {
+		if (event.data.search('modal') !== -1) {
+			var parser = new DOMParser;
+			var dom = parser.parseFromString('<!doctype html><body>' + event.data,'text/html');
+			var modal_string = dom.body.textContent;
+			var modal_container = document.createElement('div');
+			modal_container.innerHTML = modal_string;
+			var body = document.body;
+			body.appendChild(modal_container);
+			var modal = document.getElementById('st-modal');
+			appearElement(modal);
+			body.classList.add('st-modal-open')
+			document.querySelectorAll("[data-dismiss='modal']").forEach(function(elem) {
+				elem.addEventListener("click", closeDeleteModal);
+			});
+		}
+	}
+};
+//add site event listener for html from shelftaught through iframe
+window.addEventListener("message", receiveMessage);
--- iframe-setup.js
+++ iframe-setup.js
@@ -1,12 +1,21 @@
 function setupDynamicIframe() {
 	[
 	  'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js',
-	  'https://cdn.jsdelivr.net/gh/duncangarde/shelftaught-iframe-js@1.1/iframe-load-actions.js'
+	  'https://cdn.jsdelivr.net/gh/duncangarde/shelftaught-iframe-js@2.0/iframe-load-actions.js',
 	].forEach(function(src) {
 	  var script = document.createElement('script');
 	  script.src = src;
 	  script.async = false;
 	  document.head.appendChild(script);
+	});
+	[
+	  'https://cdn.jsdelivr.net/gh/duncangarde/shelftaught-iframe-js@2.0/modal.css'
+	].forEach(function(href) {
+	  var link = document.createElement('link');
+	  link.href = href;
+	  link.rel = "stylesheet"
+	  link.async = false;
+	  document.head.appendChild(link);
 	});
 }
 
