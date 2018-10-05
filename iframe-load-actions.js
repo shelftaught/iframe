7bdccf3f8baf0efe6eb4bee345f57a1306d53cf9
duncangarde <duncangarde@hotmail.com>
2018-10-05 11:03:54 +0100

    hopefully fixing multiple script loading into head, also lazy load covers

--- iframe-load-actions.js
+++ iframe-load-actions.js
@@ -1,18 +1,10 @@
-// resize iframe and send dimensions to iframe to send on to show dialogs
-iFrameResize({
-	resizedCallback: function ({iframe,height,width,type}) {
-		iframe.contentWindow.postMessage({type: 'event_trigger', 
-			event: 'set_book_review_iframe_dimensions', 
-			iframe_height: height, 
-			iframe_width: width},
-			'*');
-		}
-	});
 // send message to trigger lazy loading of covers in iframes
-var iframes = document.querySelectorAll("iframe");
-var i;
-for (i = 0; i < iframes.length; i++) {
-    iframes[i].contentWindow.postMessage({type: 'event_trigger', event: 'LazyLoadCovers'},'*')
+function iFrameLazyLoad () {
+	var iframes = document.querySelectorAll('[data-class="st-iframe"]');
+	var i;
+	for (i = 0; i < iframes.length; i++) {
+	    iframes[i].contentWindow.postMessage({type: 'event_trigger', event: 'LazyLoadCovers'},'*')
+	};
 };
 // functions for modal in site browser not in iframe
 // fade in element (modal)
@@ -33,8 +25,7 @@ function closeDeleteModal () {
   	document.body.classList.remove('st-modal-open')
 }
 // receive html from shelftaught through iframe postmessage
-function receiveMessage(event) {
-	console.log(event.origin);
+function stReceiveMessage(event) {
 	if (event.origin === "https://shelftaught.com") {
 		if (event.data.search('modal') !== -1) {
 			var parser = new DOMParser;
@@ -53,5 +44,18 @@ function receiveMessage(event) {
 		}
 	}
 };
+// resize iframe and send dimensions to iframe to send on to show dialogs
+iFrameResize();
 //add site event listener for html from shelftaught through iframe
-window.addEventListener("message", receiveMessage);
+window.addEventListener("message", stReceiveMessage);
+//lazyload covers through iframe message.
+iFrameLazyLoad();
+// iFrameResize({
+// 	resizedCallback: function ({iframe,height,width,type}) {
+// 		iframe.contentWindow.postMessage({type: 'event_trigger', 
+// 			event: 'set_book_review_iframe_dimensions', 
+// 			iframe_height: height, 
+// 			iframe_width: width},
+// 			'*');
+// 		}
+// 	});
--- iframe-setup.js
+++ iframe-setup.js
@@ -1,23 +1,37 @@
 function setupDynamicIframe() {
 	[
 	  'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js',
-	  'https://cdn.jsdelivr.net/gh/duncangarde/shelftaught-iframe-js@2.0/iframe-load-actions.js',
+	  'https://cdn.jsdelivr.net/gh/duncangarde/shelftaught-iframe-js@2.1/iframe-load-actions.js',
 	].forEach(function(src) {
-	  var script = document.createElement('script');
-	  script.src = src;
-	  script.async = false;
-	  document.head.appendChild(script);
+	  if (document.querySelectorAll('[src="' + src + '"]').length > 0) {
+	  	if (typeof iFrameResize === 'function') {
+	  		iFrameResize();
+	  	};
+	  	if (typeof stReceiveMessage === 'function') {
+	  		window.addEventListener("message", stReceiveMessage);
+	  	};
+	  	if (typeof iFrameLazyLoad === 'function') {
+	  		iFrameLazyLoad();
+	  	};
+	  }
+	  else {
+		  var script = document.createElement('script');
+		  script.src = src;
+		  script.async = false;
+		  document.head.appendChild(script);
+	  };
 	});
 	[
-	  'https://cdn.jsdelivr.net/gh/duncangarde/shelftaught-iframe-js@2.0/modal.css'
+	  'https://cdn.jsdelivr.net/gh/duncangarde/shelftaught-iframe-js@2.1/modal.css'
 	].forEach(function(href) {
-	  var link = document.createElement('link');
-	  link.href = href;
-	  link.rel = "stylesheet"
-	  link.async = false;
-	  document.head.appendChild(link);
+	  if (document.querySelectorAll('[href="' + href +'"]').length > 0) {
+	  }
+	  else {
+		  var link = document.createElement('link');
+		  link.href = href;
+		  link.rel = "stylesheet"
+		  link.async = false;
+		  document.head.appendChild(link);
+	  };
 	});
 }
-
-
-      
